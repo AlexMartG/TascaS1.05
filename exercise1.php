96de0f69837b51d2cd550879d81a5b9c9642aa90
AlexMartG <amartinezg2@hotmail.com>
2026-05-20 13:18:22 +0200

    Level1 exercise1 completed

--- exercise1.php
+++ exercise1.php
@@ -3,9 +3,34 @@
 interface AnimalSound {
     public function makeSound();
 }
+abstract class Animal implements AnimalSound {
+    protected $name;
 
+    public function __construct($name) {
+        $this->name = $name;
+    }
 
+    public function getName() {
+        return $this->name;
+    }
+    abstract public function makeSound();
+}
+class Dog extends Animal {
+    public function makeSound() {
+        return "Guau guau!";
+    }
+}
+class Cat extends Animal {
+    public function makeSound() {
+        return "Meoooow!";
+    }
+}
+$dog = new Dog("Perrito");
+$cat = new Cat("Gatito");
 
+echo $dog->getName() . " says: " . $dog->makeSound() . "\n";
+echo "<br>";
+echo $cat->getName() . " says: " . $cat->makeSound()
 
 
 ?>
